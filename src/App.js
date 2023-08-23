3c8aa34262f80b05a28b106a04e4cb517934da50
Hariharanj10 <hariharan03102001@gmail.com>
2023-08-23 17:16:15 +0530

    buildApp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sample from 'icons/Sample.js';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Webpack App</h1>
+    
+    <Sample></Sample>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/icons/Sample.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Sample = () => {
+  return (
+
+    <img src="https://img.freepik.com/free-vector/sticker-template-cat-cartoon-character_1308-73786.jpg?w=2000"></img>
+  );
+};
+
+export default Sample;
